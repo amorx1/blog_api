73c91f1951ca5846509f78e3ac37317801841511
amorx1 <86209570+amorx1@users.noreply.github.com>
2023-09-21 15:23:00 +1200

    refactoring

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -46,22 +46,20 @@ namespace BlogAPI.Controllers
 		[HttpGet("user/{userId}/posts/{postId}"), OwnerVerificationFilter]
 		public override async Task<ActionResult<PostReadDto?>> GetAsync([FromRoute] int userId, [FromRoute] int postId, bool isOwner)
 		{
-			var post = await _postRepository.GetAsync(postId);
 
+			var post = await _postRepository.GetAsync(postId);
 			if (post is null)
 			{
 				return NotFound();
 			}
-			
+
 			if (post.IsPrivate && !isOwner)
 			{
 				return Unauthorized();
 			}
-			else
-			{
-				return _mapper.Map<PostReadDto>(post);
-			}
 			
+			return _mapper.Map<PostReadDto>(post);
+					
 		}
 		
 		[HttpDelete("user/{userId}/posts/{postId}")]
@@ -71,15 +69,13 @@ namespace BlogAPI.Controllers
 			{
 				return Unauthorized();
 			}
-			else
+
+			var deletedPost = await _postRepository.DeleteAsync(postId);
+			if (deletedPost is null)
 			{
-				var deletedPost = await _postRepository.DeleteAsync(postId);
-				if (deletedPost is null)
-				{
-					return NotFound();
-				}
-				return Ok(_mapper.Map<PostReadDto>(deletedPost));
+				return NotFound();
 			}
+			return Ok(_mapper.Map<PostReadDto>(deletedPost));
 		}
 
 		[HttpPut("user/{userId}/posts/{postId}")]
@@ -89,43 +85,40 @@ namespace BlogAPI.Controllers
 			{
 				return Unauthorized();
 			}
-			else
+
+			var postToUpdate = await _postRepository.GetAsync(postId);
+			if (postToUpdate is null)
 			{
-				var postToUpdate = await _postRepository.GetAsync(postId);
-				if (postToUpdate is null)
-				{
-					return NotFound();
-				}
-				
-				if (request.Title is not null)
-				{
-					postToUpdate.Title = request.Title;
-				}
-				if (request.Body is not null)
-				{
-					postToUpdate.Body = request.Body;
-				}
-				if (request.Images is not null)
-				{
-					postToUpdate.Images = request.Images.Select(i => _mapper.Map<ImageEntity>(i)).ToList();
-				}
-
-				postToUpdate.IsPrivate = request.IsPrivate;				
-				var updatedPost = await _postRepository.UpdateAsync(postToUpdate);
-
-				return Ok(_mapper.Map<PostReadDto>(updatedPost));
+				return NotFound();
 			}
+			
+			if (request.Title is not null)
+			{
+				postToUpdate.Title = request.Title;
+			}
+			if (request.Body is not null)
+			{
+				postToUpdate.Body = request.Body;
+			}
+			if (request.Images is not null)
+			{
+				postToUpdate.Images = request.Images.Select(i => _mapper.Map<ImageEntity>(i)).ToList();
+			}
+			postToUpdate.IsPrivate = request.IsPrivate;				
+			
+			var updatedPost = await _postRepository.UpdateAsync(postToUpdate);
+			return Ok(_mapper.Map<PostReadDto>(updatedPost));
 		}
 
 		[HttpPost("user/{userId}/posts/new")]
 		public override async Task<ActionResult<PostReadDto?>> CreateAsync(int userId, [FromBody] PostWriteDto request, bool isOwner)
 		{
-			var newPost = _mapper.Map<PostEntity>(request);
-
 			if (!isOwner || userId != request.AuthorId)
 			{
 				return Unauthorized();
 			}
+
+			var newPost = _mapper.Map<PostEntity>(request);
 			var createdPost = await _postRepository.AddAsync(newPost);
 			
 			return Ok(_mapper.Map<PostReadDto>(createdPost));
